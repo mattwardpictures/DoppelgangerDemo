ed45714e7b8f1a31587930e0bf1bf7ae73c2a107
Christie <Christie.Desnoyer@gmail.com>
2018-11-30 17:29:43 -0500

    added new controllerfor new API

--- src/main/java/com/facextest/DoppelgangerDemo/FaceController.java
+++ src/main/java/com/facextest/DoppelgangerDemo/FaceController.java
@@ -22,33 +22,53 @@ public class FaceController {
 	@Value("${rapidapi.key}")
 	String faceKey;
 
+//	@RequestMapping("/")
+//	public ModelAndView compareFace() {
+//
+//		RestTemplate rt = new RestTemplate();
+//		HttpHeaders headers = new HttpHeaders();
+//		headers.add("X-RapidAPI-Key", faceKey);
+//		headers.add("user_id", "7041acf0f54c5cf35f63");
+//		headers.add("user_key", "489896a4897f8b99af2c");
+//		headers.add("Content-Type", MediaType.APPLICATION_FORM_URLENCODED_VALUE);
+//
+//		Map<String, String> params = new HashMap<>();
+//		params.put("img_1",
+//				"https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Mohanlal_Viswanathan_Nair_BNC.jpg/240px-Mohanlal_Viswanathan_Nair_BNC.jpg");
+//		params.put("img_2",
+//				"https://www.thenewsminute.com/sites/default/files/styles/news_detail/public/Mohanlal_DN_0.jpg?itok=rosZJnyx");
+//		
+//		
+//		
+//		HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+//		String url = "https://facex-facex-v1.p.rapidapi.com/compare_faces?bboxes_1=50%2C100%2C150%2C200&bboxes_2=52%2C102%2C150%2C200&face_det=1";
+//
+//		ResponseEntity<FaceResult> response = rt.exchange(url, HttpMethod.POST, entity, FaceResult.class, params);
+//
+//		FaceResult faceMatch = response.getBody();
+//
+//		System.out.println(faceMatch);
+//
+//		return new ModelAndView("index", "face", faceMatch);
+//
+//	}
+	
+	
 	@RequestMapping("/")
 	public ModelAndView compareFace() {
 
 		RestTemplate rt = new RestTemplate();
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("X-RapidAPI-Key", faceKey);
-		headers.add("user_id", "7041acf0f54c5cf35f63");
-		headers.add("user_key", "489896a4897f8b99af2c");
-		headers.add("Content-Type", MediaType.APPLICATION_FORM_URLENCODED_VALUE);
-
-		Map<String, String> params = new HashMap<>();
-		params.put("img_1",
-				"https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Mohanlal_Viswanathan_Nair_BNC.jpg/240px-Mohanlal_Viswanathan_Nair_BNC.jpg");
-		params.put("img_2",
-				"https://www.thenewsminute.com/sites/default/files/styles/news_detail/public/Mohanlal_DN_0.jpg?itok=rosZJnyx");
-
+		headers.add("Ocp-Apim-Subscription-Key", "12644903-ab3b-4aa9-ac87-33121753cc06");
+		headers.add("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+		
 		HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
-		String url = "https://facex-facex-v1.p.rapidapi.com/compare_faces?bboxes_1=50%2C100%2C150%2C200&bboxes_2=52%2C102%2C150%2C200&face_det=1";
-
+		String url = "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize\";
+		
 		ResponseEntity<FaceResult> response = rt.exchange(url, HttpMethod.POST, entity, FaceResult.class, params);
-
-		FaceResult faceMatch = response.getBody();
-
-		System.out.println(faceMatch);
-
+		
+		
 		return new ModelAndView("index", "face", faceMatch);
-
 	}
-
 }
