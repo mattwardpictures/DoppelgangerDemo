90c8ae0d26c6afccd919f5e3fd5cfcd84d6be86f
mattwardpictures <mattwardpictures@gmail.com>
2018-11-30 14:36:28 -0500

    finally got the damn api working

--- src/main/java/com/facextest/DoppelgangerDemo/FaceController.java
+++ src/main/java/com/facextest/DoppelgangerDemo/FaceController.java
@@ -11,11 +11,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.facextest.DoppelgangerDemo.entity.FaceMatcher;
+import com.facextest.DoppelgangerDemo.entity.FaceResult;
 
 @Controller
 public class FaceController {
@@ -23,18 +22,15 @@ public class FaceController {
 	@Value("${rapidapi.key}")
 	String faceKey;
 
-	@RequestMapping("index")
-	public ModelAndView compareFace(@RequestParam("user_id") String user_id, @RequestParam("user_key") String user_key,
-			@RequestParam("img_1") String img_1, @RequestParam("img_2") String img_2,
-			@RequestParam("face_det") int face_det) {
+	@RequestMapping("/")
+	public ModelAndView compareFace() {
 
 		RestTemplate rt = new RestTemplate();
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("X-RapidAPI-Key", faceKey);
-		headers.add(user_id, "7041acf0f54c5cf35f63");
-		headers.add(user_key, "489896a4897f8b99af2c");
+		headers.add("user_id", "7041acf0f54c5cf35f63");
+		headers.add("user_key", "489896a4897f8b99af2c");
 		headers.add("Content-Type", MediaType.APPLICATION_FORM_URLENCODED_VALUE);
-		face_det = 1;
 
 		Map<String, String> params = new HashMap<>();
 		params.put("img_1",
@@ -43,12 +39,11 @@ public class FaceController {
 				"https://66.media.tumblr.com/81bdaed659fbd2cb32362b766af44e02/tumblr_n5vjnxBU8v1rjw5kqo1_500.png");
 
 		HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
-		String url = "https://facex-facex-v1.p.rapidapi.com/compare_faces?bboxes_1=50%2C100%2C150%2C200&bboxes_2=52%2C102%2C150%2C200&face_det="
-				+ face_det;
+		String url = "https://facex-facex-v1.p.rapidapi.com/compare_faces?bboxes_1=50%2C100%2C150%2C200&bboxes_2=52%2C102%2C150%2C200&face_det=1";
 
-		ResponseEntity<FaceMatcher> response = rt.exchange(url, HttpMethod.POST, entity, FaceMatcher.class, params);
+		ResponseEntity<FaceResult> response = rt.exchange(url, HttpMethod.POST, entity, FaceResult.class, params);
 
-		FaceMatcher faceMatch = response.getBody();
+		FaceResult faceMatch = response.getBody();
 
 		System.out.println(faceMatch);
 
